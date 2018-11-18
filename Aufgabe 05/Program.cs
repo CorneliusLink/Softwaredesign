2fd3d6101cab7b3f559ec4a2e40e012f90c5768c
CorneliusLink <linkco@gmx.de>
2018-11-18 13:06:57 +0100

    Add Aufgabe 5 to repo

---
+++ Aufgabe 05/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+namespace Aufgabe_05
+{
+    class Program
+    {
+        static StringBuilder wordsbackwards = new StringBuilder("");
+
+        static void Main(string[] args) {
+            string sentence = "Hallo Welt";
+            string[] words = sentence.Split(" ");
+            Console.WriteLine("Sentence: " + sentence);
+            Console.WriteLine("Backwards per Letter: " + backwardsPerLetter(sentence));
+            Console.WriteLine("Backwards per Word: " + backwardsPerWord(words));
+            Console.WriteLine("Words Backwards: " + wordsbackwards);
+        }
+
+        public static string backwardsPerLetter(string sentence)
+        {
+            StringBuilder letters = new StringBuilder("");
+
+            for (int i = sentence.Length-1; i >= 0; i--)
+            {
+                letters.Append(sentence[i]);
+            }
+
+            return letters.ToString();
+        }
+
+        public static string backwardsPerWord(string[] words)
+        {
+            StringBuilder bPword = new StringBuilder("");
+
+            for (int i = words.Length-1; i >= 0; i--)
+            {
+                bPword.Append(words[i]);
+                wordsbackwards.Append(backwardsPerLetter(words[i] + " "));
+            }
+
+            // wordsbackwards.Length--; Required?
+
+            return bPword.ToString();
+        }
+    }
+}
